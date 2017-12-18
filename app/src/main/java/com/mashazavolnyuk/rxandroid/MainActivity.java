d3b00c8dcdbb0c9e5f8e0bdf5727b70c688d9af6
DESKTOP-0SQTNBI\user <mashazavolnyuk@yandex.ru>
2017-12-18 23:49:18 +0200

    Added SimpleStringAdapter. Implemented in activity.

--- app/src/main/java/com/mashazavolnyuk/rxandroid/MainActivity.java
+++ app/src/main/java/com/mashazavolnyuk/rxandroid/MainActivity.java
@@ -2,12 +2,28 @@ package com.mashazavolnyuk.rxandroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import com.mashazavolnyuk.rxandroid.adapters.SimpleStringAdapter;
 
 public class MainActivity extends AppCompatActivity {
+
+    RecyclerView mColorListView;
+    SimpleStringAdapter mSimpleStringAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        configureLayout();
+    }
+
+    private void configureLayout() {
+        setContentView(R.layout.activity_main);
+        mColorListView = findViewById(R.id.color_list);
+        mColorListView.setLayoutManager(new LinearLayoutManager(this));
+        mSimpleStringAdapter = new SimpleStringAdapter(this);
+        mColorListView.setAdapter(mSimpleStringAdapter);
     }
 }
