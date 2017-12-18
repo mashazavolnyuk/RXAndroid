3a28feb2a23519d91ffef0c4a0f8c9c4e9496e81
DESKTOP-0SQTNBI\user <mashazavolnyuk@yandex.ru>
2017-12-19 00:06:06 +0200

    Added exmaple with loading color list.

--- app/src/main/java/com/mashazavolnyuk/rxandroid/MainActivity.java
+++ app/src/main/java/com/mashazavolnyuk/rxandroid/MainActivity.java
@@ -7,6 +7,14 @@ import android.support.v7.widget.RecyclerView;
 
 import com.mashazavolnyuk.rxandroid.adapters.SimpleStringAdapter;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import io.reactivex.Observable;
+import io.reactivex.Observer;
+import io.reactivex.disposables.Disposable;
+
+
 public class MainActivity extends AppCompatActivity {
 
     RecyclerView mColorListView;
@@ -17,6 +25,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         configureLayout();
+        createObservable();
     }
 
     private void configureLayout() {
@@ -25,5 +34,42 @@ public class MainActivity extends AppCompatActivity {
         mColorListView.setLayoutManager(new LinearLayoutManager(this));
         mSimpleStringAdapter = new SimpleStringAdapter(this);
         mColorListView.setAdapter(mSimpleStringAdapter);
+    }
+
+    private void createObservable() {
+        Observable<List<String>> listObservable = Observable.just(getColorList());
+
+        listObservable.subscribe(new Observer<List<String>>() {
+
+            @Override
+            public void onError(Throwable e) {
+
+            }
+            @Override
+            public void onComplete() {
+
+            }
+
+            @Override
+            public void onSubscribe(Disposable d) {
+
+            }
+
+            @Override
+            public void onNext(List<String> colors) {
+                mSimpleStringAdapter.setStrings(colors);
+            }
+        });
+
+    }
+
+    private static List<String> getColorList() {
+        ArrayList<String> colors = new ArrayList<>();
+        colors.add("blue");
+        colors.add("green");
+        colors.add("red");
+        colors.add("chartreuse");
+        colors.add("Van Dyke Brown");
+        return colors;
     }
 }
